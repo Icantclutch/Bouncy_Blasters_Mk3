baa82e2f02ee539fec8c18e24a0dbabd73f38b85
Icantclutch <54913583+Icantclutch@users.noreply.github.com>
2020-10-07 00:13:46 -0500

    Unfinished and Incorrect Physics
    Working on the glass refraction scripts.
    they don't work. Trying to upgrade 2D physics to 3D is difficult and I am taking a break.

--- Assets/Scripts/GlassRefraction.cs
+++ Assets/Scripts/GlassRefraction.cs
@@ -18,5 +18,43 @@ public class GlassRefraction : MonoBehaviour
         
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        Vector3 initialBulletVelocity;
+        Vector3 glassBulletVelocity;
+        Vector3 contactPoint;
+        Vector3 glassNormal;
+        float thetaIn;
+        float phiIn;
+        float thetaOut;
+        float phiOut;
+
+        if (other.tag == "Bullet")
+        {
+            initialBulletVelocity = other.GetComponent<Rigidbody>().velocity;
+            contactPoint = other.gameObject.GetComponent<Collider>().ClosestPointOnBounds(transform.position);
+            if (contactPoint.z < 0)
+            {
+                glassNormal = new Vector3(0, 0, -1);
+            }
+            else if (contactPoint.z > 0)
+            {
+                glassNormal = new Vector3(0, 0, 1);
+            }
+            else
+            {
+                glassNormal = Vector3.zero;
+                Debug.LogWarning("Bullet Contact Point was not detected on one side or the other and thus the contact point normal could not be determined");
+            }
+
+            thetaIn = Mathf.Rad2Deg * Mathf.Acos((initialBulletVelocity.z * glassNormal.z) / (initialBulletVelocity.magnitude));
+            Debug.Log(thetaIn);
+
+
+            //Take Velocity
+            //Calculate Theta
+        }
+    }
+
    
 }
